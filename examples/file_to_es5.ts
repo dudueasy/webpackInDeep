b09f35c19a81716ab9d95e94959b23c482bd78e1
apolodu <dudueasy@126.com>
2021-07-21 02:45:23 +0800

    feat: update file to es5 example

--- examples/file_to_es5.ts
+++ examples/file_to_es5.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 // 为了保证 node 运行结果的一致性 (启动 node 的位置可能不同), 使用绝对路径
-const filePath = path.join(__dirname, './test.js')
+const filePath = path.resolve(__dirname, './test.js')
 console.log(`__dirname`, __dirname)
 const code = fs.readFileSync(filePath).toString();
 const ast = parse(code, { sourceType: 'module'});
