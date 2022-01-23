53ef52904b32e810f17d916785147d5e3e12c6ea
apolodu <dudueasy@126.com>
2022-01-24 01:31:09 +0800

    feat: 实现打包能力1 - 依赖分析结果改为数组

---
+++ src/Chapter2/bundler.ts
@@ -0,0 +1,61 @@
+import {parse} from "@babel/parser"
+import traverse from "@babel/traverse"
+import {transform} from "@babel/core";
+import {readFileSync} from 'fs'
+import {resolve, relative, dirname} from 'path';
+
+const PROJECT_DIR_NAME = 'project'
+const ENTRY_FILE = 'index.js';
+
+type DepRelation = { key: string, deps: string[], code: string };
+
+const projectRoot = resolve(__dirname, PROJECT_DIR_NAME);
+const entryPath = resolve(projectRoot, ENTRY_FILE);
+
+const depRelation: DepRelation[] = [];
+collectCodeAndDeps(entryPath);
+
+console.log(depRelation);
+console.log('done');
+
+function collectCodeAndDeps(filePath: string): DepRelation {
+    const key = relative(projectRoot, filePath)
+
+    // 一旦检查到这个文件名已经收集过依赖,  就退出, 从而解决循环依赖
+    if (depRelation.find((item) => item.key === key)) {
+        return
+    }
+
+    const code = readFileSync(filePath, 'utf8');
+    const {code: es5Code} = transform(code, {presets: ['@babel/preset-env']});
+
+    const ast = parse(code, {sourceType: 'module'});
+
+    // init depRelation[key]
+    const item = {key, deps: [], code: es5Code}
+    // 先 push, 才能保证入口文件顺序
+    depRelation.push(item)
+
+    traverse(ast, {
+        enter(path) {
+
+
+            if (path.node.type === 'ImportDeclaration') {
+                // 依赖相对当前文件的位置
+                const importedFilePath = path.node.source.value
+                // 依赖的绝对位置
+                const absoluteImportFilePath = resolve(dirname(filePath), importedFilePath)
+                console.log(`importedFilePath `, importedFilePath)
+                console.log(`absoluteImportFilePath `, absoluteImportFilePath)
+                // 依赖相对于projectRoot 的位置
+                const relativeImportFilePath = relative(projectRoot, absoluteImportFilePath)
+
+                item.deps.push(relativeImportFilePath)
+
+                // depRelation[key].deps.push(relativeImportFilePath)
+                collectCodeAndDeps(absoluteImportFilePath);
+            }
+        },
+    })
+
+}
